dd97720432945a8a05ff6edcca22465f0ec4f5a4
mp <maulikk95@gmail.com>
2025-10-16 11:04:47 -0400

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,9 @@ const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   output: 'export',
-  images: { unoptimized: true }, // Keep for static export
-
-  // No basePath or assetPrefix needed for username.github.io repo
+  images: { unoptimized: true }, // static export
 };
 
-const withMDX = createMDX({
+export default createMDX({
   extension: /\.mdx?$/,
-});
-
-export default withMDX(nextConfig);
+})(nextConfig);
